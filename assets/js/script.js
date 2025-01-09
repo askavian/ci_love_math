28e77f515142d3c910e05e28e95e40abc70d81d6
askavian <malte.boettcher@live.com>
2025-01-09 13:59:43 +0100

    feat: js added placeholder functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+    console.log("runGame empty!");
+}
+
+function checkAnswer() {
+    console.log("checkAnswer empty!")
+}
+
+function calculateCorrectAnswer() {
+    console.log("calculateScore empty!")
+}
+
+function incrementScore() {
+    console.log("incrementScore empty!")
+}
+
+function incrementWrongAnswer() {
+    console.log("incrementWrongAnswer empty!")
+}
+
+function displayAdditionQuestion() {
+    console.log("displayAdditionQuestion empty!")
+}
+
+function displaySubtractQuestion() {
+    console.log("displaySubtractQuestion empty!")
+}
+
+function displayMultiplyQuestion() {
+    console.log("displayMultiplyQuestion empty!")
+}
