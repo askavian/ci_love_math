1dd46c030b0833cfe6eab6b7be45264eef8297de
askavian <malte.boettcher@live.com>
2025-01-09 14:54:14 +0100

    feat: js event listeners added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,60 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Event Listener returns all buttons on page as array (4 game button, 1 submit button)
+    let buttons = document.getElementsByTagName("button");
+
+    // 1of2 methond for iterating through array NOT used in this
+    // for (let i = 0; i < buttons.length; i++); {
+    //
+    //}
+
+    // 2of2 modern version because index notation is not neccessary when navigating array
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
-    console.log("runGame empty!");
+    
 }
 
 function checkAnswer() {
-    console.log("checkAnswer empty!")
+    
 }
 
 function calculateCorrectAnswer() {
-    console.log("calculateScore empty!")
+         
 }
 
 function incrementScore() {
-    console.log("incrementScore empty!")
+    
 }
 
 function incrementWrongAnswer() {
-    console.log("incrementWrongAnswer empty!")
+    
 }
 
 function displayAdditionQuestion() {
-    console.log("displayAdditionQuestion empty!")
+    
 }
 
 function displaySubtractQuestion() {
-    console.log("displaySubtractQuestion empty!")
+    
 }
 
 function displayMultiplyQuestion() {
-    console.log("displayMultiplyQuestion empty!")
+    
 }
+
+
+// DELETE
+console.log("END TEST");
