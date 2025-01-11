7a752caf01515ee3682e76f8a187f25bc450e5b6
askavian <malte.boettcher@live.com>
2025-01-11 14:54:06 +0100

    feat: js division game added

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,7 +160,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand1").textContent = operand1 * operand2; // ensures the result is always a whole integer number
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
     
